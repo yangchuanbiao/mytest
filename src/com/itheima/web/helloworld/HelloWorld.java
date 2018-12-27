fed37f69786c6527e8ed5e7c84111f3d71abe5d7
yangbiao <790208672@qq.com>
2018-12-28 00:53:02 +0800

    33333

--- src/com/itheima/web/helloworld/HelloWorld.java
+++ src/com/itheima/web/helloworld/HelloWorld.java
@@ -13,12 +13,7 @@ public class HelloWorld {
 		System.out.println(2);
 		System.out.println(3);
 		// int i = 1 / 1;
-		System.out.println(4);
-		System.out.println(5);
-		System.out.println(333);
-		System.out.println(333333);
-		System.out.println(111);
-		System.out.println(222);
+	
 		return "index";
 	}
 	
